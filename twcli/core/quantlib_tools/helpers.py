c86c5a21b06cb5f8885f42b96b59df5c105492a9
Joe D <dasenbrockjw@gmail.com>
2021-05-13 13:54:01 -0500

    Added quantlib tools package, requirements
    Added quantlib_tools package and modified requirements for support.
    
    Full testing is not yet configured, but was able to successfully pass
    test cases present in twcli/core/quantlib_tools/test_models.py.
    
    An error was raised when trying to build the environment for testing,
    the package pytest and a couple of others were listed in both
    requirements.txt and requirements-dev.txt. The duplicated packages were
    removed, which suppressed the error.

---
+++ twcli/core/quantlib_tools/helpers.py
@@ -0,0 +1,16 @@
+"""Misc functions"""
+import QuantLib as ql
+import numpy as np
+
+
+def summarize_params(price_underlying, strike, interest, volatility, date_exp, 
+                     date_evl):
+    """Display values that are inputs to option price"""
+    msg = (f"* {price_underlying.value()} - underlying price\n"
+           f"* {strike} - strike\n"
+           f"* {interest} - interest\n"
+           f"* {volatility.value()} - volatility\n"
+           f"* {date_exp} - exp date\n"
+           f"* {date_evl} - eval date\n")
+
+    print(msg)
