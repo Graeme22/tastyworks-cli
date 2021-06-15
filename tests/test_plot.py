5abd0ca7c89ef7399aef4145b08a4d9f4023f0b1
Graeme22 <graeme.holliday@pm.me>
2021-06-15 11:33:38 -0600

    fix nondeterministic tests

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -4,9 +4,9 @@ from src.plot.csv import _DURATIONS, Portfolio
 
 _CSV_PATH = 'tests/data/transactions.csv'
 _NET_LIQUIDITY = 6645.84
-_NET_LIQUIDITY_PERCENT = 87.41
-_REALIZED_PANDL = -588.58
-_REALIZED_PANDL_PERCENT = -14.56
+_NET_LIQUIDITY_PERCENT = 199.09
+_REALIZED_PANDL = -2186.87
+_REALIZED_PANDL_PERCENT = -98.42
 
 
 def gen_portfolio(net_liq=False):
@@ -19,7 +19,7 @@ def gen_portfolio(net_liq=False):
 
 def test_plot_pandl():
     pf = gen_portfolio()
-    val = pf.plot('ytd')
+    val = pf.plot('all')
 
     assert round(val, 2) == _REALIZED_PANDL
 
@@ -37,7 +37,7 @@ def test_plot_pandl_percent():
     pf = gen_portfolio()
     pf_tmp = gen_portfolio(True)
     nl = pf_tmp._get_starting_net_liq('1y')
-    val = pf.plot('1y', starting_net_liq=nl)
+    val = pf.plot('all', starting_net_liq=nl)
 
     assert round(val, 2) == _REALIZED_PANDL_PERCENT
 
@@ -46,7 +46,7 @@ def test_plot_netliq_percent():
     pf = gen_portfolio(True)
     pf_tmp = gen_portfolio(True)
     nl = pf_tmp._get_starting_net_liq('6m')
-    val = pf.plot('6m', starting_net_liq=nl)
+    val = pf.plot('all', starting_net_liq=nl)
 
     assert round(val, 2) == _NET_LIQUIDITY_PERCENT
 
